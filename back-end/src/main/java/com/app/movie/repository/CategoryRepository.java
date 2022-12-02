5511d6e5d4823858a57eb5ecda77786b0df88885
Santiago Velasco <santiagovelascomosquera@hotmail.com>
2022-12-02 09:34:33 -0500

    fix Category Repository

--- back-end/src/main/java/com/app/movie/repository/CategoryRepository.java
+++ back-end/src/main/java/com/app/movie/repository/CategoryRepository.java
@@ -1,26 +1,12 @@
-<<<<<<<< HEAD:sprint2/app-streaming/src/main/java/com/streaming/appstreaming/repository/CategoryRepository.java
-package com.streaming.appstreaming.repository;
-========
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.app.movie.repository;
->>>>>>>> 82658319d37a3833ca5bf9df0d6d7226aa713d09:back-end/src/main/java/com/app/movie/repository/CategoryRepository.java
 
 import com.app.movie.entities.Category;
 import com.app.movie.interfaces.ICategoryRepository;
 
-<<<<<<<< HEAD:sprint2/app-streaming/src/main/java/com/streaming/appstreaming/repository/CategoryRepository.java
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.streaming.appstreaming.entities.Category;
-import com.streaming.appstreaming.interfaces.ICategoryRepository;
-
-import java.util.Optional;
-
-========
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -29,7 +15,6 @@ import org.springframework.stereotype.Repository;
  *
  * @author Andres
  */
->>>>>>>> 82658319d37a3833ca5bf9df0d6d7226aa713d09:back-end/src/main/java/com/app/movie/repository/CategoryRepository.java
 @Repository
 public class CategoryRepository {
     
