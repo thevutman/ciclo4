e3ef36166a4292ba6fda2490f966dade8bd3250f
Santiago Velasco <santiagovelascomosquera@hotmail.com>
2022-12-02 09:33:48 -0500

    fix Client Repository

--- back-end/src/main/java/com/app/movie/repository/ClientRepository.java
+++ back-end/src/main/java/com/app/movie/repository/ClientRepository.java
@@ -1,17 +1,3 @@
-<<<<<<<< HEAD:sprint2/app-streaming/src/main/java/com/streaming/appstreaming/repository/ClientRepository.java
-package com.streaming.appstreaming.repository;
-
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.streaming.appstreaming.entities.Client;
-import com.streaming.appstreaming.interfaces.IClientRepository;
-
-import java.util.Optional;
-
-========
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -29,7 +15,6 @@ import org.springframework.stereotype.Repository;
  *
  * @author Andres
  */
->>>>>>>> 82658319d37a3833ca5bf9df0d6d7226aa713d09:back-end/src/main/java/com/app/movie/repository/ClientRepository.java
 @Repository
 public class ClientRepository {
     
