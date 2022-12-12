037175478fa331112ff64c87040a90f92bd550df
Santiago Velasco <santiagovelascomosquera@hotmail.com>
2022-12-12 12:12:46 -0500

    [front-end] se actualizo -se agregaron carpetas para un trabajo mas facil

---
+++ front-end/src/components/Cajitas.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/cajitas.scss'
+
+const Cajitas = () => {
+    return (
+        <div className='cajitas'>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+            <div className='cajitas__items'></div>
+        </div>
+
+    );
+};
+
+export default Cajitas;
---
+++ front-end/src/components/Prueba.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Prueba.scss";
+import Cajitas from './Cajitas';
+
+const Prueba = () => {
+    return (
+        <div className='contenedor'>
+            <Cajitas />
+        </div>
+    );
+};
+
+export default Prueba;
