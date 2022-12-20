e3f6560c80b63b07298febc28be328a49ac792ec
savendanog <emprendesym@gmail.com>
2022-12-20 10:50:31 -0500

    Adición archivo Category.js

---
+++ front-end/src/components/Category.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div>
+            <div>
+                <h2>Películas</h2>
+                <select name='Category'>
+                    <option>
+                        valor 1
+                    </option>
+                    <option>
+                        valor 2
+                    </option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default Category;
